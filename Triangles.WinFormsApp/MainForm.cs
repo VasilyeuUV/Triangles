aad0d8d336bd5de4e8ad771c873a3936102072f2
VasilyeuUV <vasilyeu.uv@mail.ru>
2023-07-13 05:32:39 +0300

    winforms: create project, add user dialog service

---
+++ Triangles.WinFormsApp/MainForm.cs
@@ -0,0 +1,26 @@
+namespace Triangles.WinFormsApp
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+
+        //#################################################################################################################
+        #region Events
+
+        /// <summary>
+        /// On form load
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        #endregion // Events
+    }
+}
---
+++ Triangles.WinFormsApp/Services/UserDialogServices/UserDialogService.cs
@@ -0,0 +1,70 @@
+﻿namespace Triangles.WinFormsApp.Services.UserDialogServices
+{
+    /// <summary>
+    /// Диалог с пользоватеем
+    /// </summary>
+    internal class UserDialogService : IUserDialog
+    {
+        public bool OpenFile(
+            string title, 
+            out string? openedFile, 
+            string filter = "Все файлы (*.*) | *.*"
+            )
+        {
+            var fileDialog = new OpenFileDialog()
+            {
+                Title = title,
+                Filter = filter
+            };
+
+            if (fileDialog.ShowDialog() == DialogResult.Cancel)
+            {
+                openedFile = null;
+                return false;
+            }
+            openedFile = fileDialog.FileName;
+            return true;
+        }
+
+
+        public bool ShowConfirm(string title, string msg, bool exclamation = false)
+            => MessageBox.Show(
+                msg,
+                title,
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Information,
+                MessageBoxDefaultButton.Button1,
+                MessageBoxOptions.DefaultDesktopOnly
+            ) == DialogResult.Yes;
+
+        public void ShowError(string title, string msg) 
+            => MessageBox.Show(
+                msg,
+                title,
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Error,
+                MessageBoxDefaultButton.Button1,
+                MessageBoxOptions.DefaultDesktopOnly
+                );
+
+        public void ShowInformation(string title, string msg) 
+            => MessageBox.Show(
+                msg,
+                title,
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information,
+                MessageBoxDefaultButton.Button1,
+                MessageBoxOptions.DefaultDesktopOnly
+                );
+
+        public void ShowWarning(string title, string msg) 
+            => MessageBox.Show(
+                msg,
+                title,
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Warning,
+                MessageBoxDefaultButton.Button1,
+                MessageBoxOptions.DefaultDesktopOnly
+                );
+    }
+}
